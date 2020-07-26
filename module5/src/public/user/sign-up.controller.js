a953e5a3ac3bf7d218ffb4e2f8c960a55701ff92
Rinoj77 <55315843+Rinoj77@users.noreply.github.com>
2020-07-26 17:47:29 +0545

    Updated

--- module5/src/public/user/sign-up.controller.js
+++ module5/src/public/user/sign-up.controller.js
@@ -1,29 +1,40 @@
 (function() {
     'use strict';
-    angular.module('public')
-        .controller('SignUpController', SignUpController);
-    SignUpController.$inject = ['UserService'];
+     
+    angular.module('public').controller('SignupController', signupController);
+    signupController.$inject = ['MenuService'];
 
-    function SignUpController(UserService) {
-        var signUpCtrl = this;
-        signUpCtrl.processForm = function(completed) {
-            if (!signUpCtrl.user) {
-                signUpCtrl.user = {};
+    var signupController = function(MenuService) {
+        var vm = this;
+
+        vm.user = {};
+        vm.favoriteDish = {};
+
+        vm.showError = false;       // When this value is true error about the favorite dish wiil be shown
+        vm.showMessage = false;     // When this value is true message about successfull signup will be shown
+
+        vm.signup = function(form) {
+            vm.showError = false;
+            vm.showMessage = false;
+            // If the form is not valid don't submit
+            if(form.$invalid) {
+                console.log('The form is not valid');
+                return;
             }
-            UserService.getUserFavMenu(signUpCtrl.user.favMenu)
-                .then(function(result) {
-                    signUpCtrl.error = false;
-                    if (completed) {
-                        UserService.setUserData(signUpCtrl.user);
-                        signUpCtrl.completed = true;
-                    } else {
-                        signUpCtrl.valid = true;
-                    }
-                }).catch(function(e) {
-                    signUpCtrl.valid = false;
-                    signUpCtrl.error = true;
-                    signUpCtrl.completed = false;
-                });
-        };
-    }
+
+            MenuService.getFavoriteDish(vm.user.favoriteDish).then(function(response) {
+                vm.user.favoriteDishDetails = response.data;
+                console.log(vm.favoriteDish);
+                MenuService.saveUser(vm.user);
+                vm.showMessage = true;
+            }, function(error) {
+                console.log(error);
+                vm.showError = true;
+            });
+
+        }
+    };
+
+
+   
 })();
